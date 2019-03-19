2db3fb627ab15b8cc8a555204133b45258fc78ec
Fabio Vitalba <fabio.vitalba@outlook.com>
2019-03-19 16:23:36 +0100

    Update main.py
    - Fixed Numer/Note conversion
    - Fixed Fretboard print
    - Removed unnecessary print messages

--- main.py
+++ main.py
@@ -36,23 +36,23 @@ def convert_note_to_number(note):
     return {
         "A": 0,
         "A#": 1,
-        "BM": 1,
+        "BB": 1,
         "B": 2,
         "B#": 3,
         "C": 3,
         "C#": 4,
-        "DM": 4,
+        "DB": 4,
         "D": 5,
         "D#": 6,
-        "EM": 6,
+        "EB": 6,
         "E": 7,
         "E#": 8,
         "F": 8,
         "F#": 9,
-        "GM": 9,
+        "GB": 9,
         "G": 10,
         "G#": 11,
-        "AM": 11,
+        "AB": 11,
     }.get(note, -1)
 
 # Convert an integer to a text (note)
@@ -75,12 +75,12 @@ def convert_number_to_note(int_note):
 # Get the Guitar String Name from an integer
 def get_guitar_note_from_int(string_no):
     return {
-        1: "E",
-        2: "A",
-        3: "D",
-        4: "G",
-        5: "B",
         6: "E",
+        5: "A",
+        4: "D",
+        3: "G",
+        2: "B",
+        1: "e",
     }.get(string_no, "Invalid String No.")
 
 def draw_fretboard(string_no, fret_no):
@@ -90,35 +90,43 @@ def draw_fretboard(string_no, fret_no):
     str_pos[string_no - 1][fret_no] = "X"
 
     # E
-    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
+    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
     get_guitar_note_from_int(1),
-    str_pos[0][0],str_pos[0][1],str_pos[0][2],str_pos[0][3],str_pos[0][4],str_pos[0][5],str_pos[0][6],str_pos[0][7],str_pos[0][8],str_pos[0][9],str_pos[0][10],str_pos[0][11]
+    #str_pos[0][0],
+    str_pos[0][1],str_pos[0][2],str_pos[0][3],str_pos[0][4],str_pos[0][5],str_pos[0][6],str_pos[0][7],str_pos[0][8],str_pos[0][9],str_pos[0][10],str_pos[0][11]
     ))
     # A
-    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
+    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
     get_guitar_note_from_int(2),
-    str_pos[1][0],str_pos[1][1],str_pos[1][2],str_pos[1][3],str_pos[1][4],str_pos[1][5],str_pos[1][6],str_pos[1][7],str_pos[1][8],str_pos[1][9],str_pos[1][10],str_pos[1][11]
+    #str_pos[1][0],
+    str_pos[1][1],str_pos[1][2],str_pos[1][3],str_pos[1][4],str_pos[1][5],str_pos[1][6],str_pos[1][7],str_pos[1][8],str_pos[1][9],str_pos[1][10],str_pos[1][11]
     ))
     # D
-    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
+    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
     get_guitar_note_from_int(3),
-    str_pos[2][0],str_pos[2][1],str_pos[2][2],str_pos[2][3],str_pos[2][4],str_pos[2][5],str_pos[2][6],str_pos[2][7],str_pos[2][8],str_pos[2][9],str_pos[2][10],str_pos[2][11]
+    #str_pos[2][0],
+    str_pos[2][1],str_pos[2][2],str_pos[2][3],str_pos[2][4],str_pos[2][5],str_pos[2][6],str_pos[2][7],str_pos[2][8],str_pos[2][9],str_pos[2][10],str_pos[2][11]
     ))
     # G
-    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
+    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
     get_guitar_note_from_int(4),
-    str_pos[3][0],str_pos[3][1],str_pos[3][2],str_pos[3][3],str_pos[3][4],str_pos[3][5],str_pos[3][6],str_pos[3][7],str_pos[3][8],str_pos[3][9],str_pos[3][10],str_pos[3][11]
+    #str_pos[3][0],
+    str_pos[3][1],str_pos[3][2],str_pos[3][3],str_pos[3][4],str_pos[3][5],str_pos[3][6],str_pos[3][7],str_pos[3][8],str_pos[3][9],str_pos[3][10],str_pos[3][11]
     ))
     # B
-    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
+    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
     get_guitar_note_from_int(5),
-    str_pos[4][0],str_pos[4][1],str_pos[4][2],str_pos[4][3],str_pos[4][4],str_pos[4][5],str_pos[4][6],str_pos[4][7],str_pos[4][8],str_pos[4][9],str_pos[4][10],str_pos[4][11]
+    #str_pos[4][0],
+    str_pos[4][1],str_pos[4][2],str_pos[4][3],str_pos[4][4],str_pos[4][5],str_pos[4][6],str_pos[4][7],str_pos[4][8],str_pos[4][9],str_pos[4][10],str_pos[4][11]
     ))
     # E
-    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
+    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
     get_guitar_note_from_int(6),
-    str_pos[5][0],str_pos[5][1],str_pos[5][2],str_pos[5][3],str_pos[5][4],str_pos[5][5],str_pos[5][6],str_pos[5][7],str_pos[5][8],str_pos[5][9],str_pos[5][10],str_pos[5][11]
+    #str_pos[5][0],
+    str_pos[5][1],str_pos[5][2],str_pos[5][3],str_pos[5][4],str_pos[5][5],str_pos[5][6],str_pos[5][7],str_pos[5][8],str_pos[5][9],str_pos[5][10],str_pos[5][11]
     ))
+    # print the help bullets of the guitar, the 12th fret bullet has to be printed as an m
+    print("        o   o   o   o   m ")
 
 print("Welcome to the Python Guitar Note Trainer.")
 print("")
@@ -129,7 +137,7 @@ while (trainer_active):
     # Define a random position on the guitar neck
     guitar_string = randint(1,6)
     guitar_fret = randint(0,12)
-    print("Please try to convert the following constellation: Guitar String: {}, Guitar Fret: {}".format(guitar_string, guitar_fret))
+    #print("Please try to convert the following constellation: Guitar String: {}, Guitar Fret: {}".format(guitar_string, guitar_fret))
     draw_fretboard(guitar_string,guitar_fret)
     print("")
     user_guess = input("What do you think this Note is: ")
