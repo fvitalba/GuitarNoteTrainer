61f01f51eac599c7b023b776140998d1f47d6381
Fabio Vitalba <fabio.vitalba@outlook.com>
2019-03-20 18:23:46 +0100

    Update main.py
    - Added help command
    - fixed a bug where the first string wasn't interpreted correctly

--- main.py
+++ main.py
@@ -87,8 +87,8 @@ def get_guitar_note_from_int(string_no):
 def draw_fretboard(string_no, fret_no):
     # Creates a list containing 5 lists, each of 8 items, all set to 0
     strings, frets = 6, 12;
-    str_pos = [["-" for x in range(frets)] for y in range(strings)]
-    str_pos[string_no - 1][fret_no] = "X"
+    str_pos = [["-".center(7,"-") for x in range(frets)] for y in range(strings)]
+    str_pos[string_no - 1][fret_no] = "X".center(7,"-")
 
     # E
     print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
@@ -126,15 +126,61 @@ def draw_fretboard(string_no, fret_no):
     #str_pos[5][0],
     str_pos[5][1],str_pos[5][2],str_pos[5][3],str_pos[5][4],str_pos[5][5],str_pos[5][6],str_pos[5][7],str_pos[5][8],str_pos[5][9],str_pos[5][10],str_pos[5][11]
     ))
-    # print the help bullets of the guitar, the 12th fret bullet has to be printed as an m
-    print("        o   o   o   o   m ")
+    # print the help bullets of the guitar
+    print("                       o               o               o               o              o o   ")
+
+def draw_fretboard_with_notes():
+    # Creates a list containing 5 lists, each of 8 items, all set to 0
+    strings, frets = 6, 12;
+    str_pos = [["-" for x in range(frets)] for y in range(strings)]
+    for i in range(strings):
+        for j in range(frets):
+            str_pos[i][j] = get_correct_guitar_note(get_guitar_note_from_int(i+1).upper(),j).center(7," ")
+    # E
+    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
+    get_guitar_note_from_int(1),
+    #str_pos[0][0],
+    str_pos[0][1],str_pos[0][2],str_pos[0][3],str_pos[0][4],str_pos[0][5],str_pos[0][6],str_pos[0][7],str_pos[0][8],str_pos[0][9],str_pos[0][10],str_pos[0][11]
+    ))
+    # A
+    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
+    get_guitar_note_from_int(2),
+    #str_pos[1][0],
+    str_pos[1][1],str_pos[1][2],str_pos[1][3],str_pos[1][4],str_pos[1][5],str_pos[1][6],str_pos[1][7],str_pos[1][8],str_pos[1][9],str_pos[1][10],str_pos[1][11]
+    ))
+    # D
+    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
+    get_guitar_note_from_int(3),
+    #str_pos[2][0],
+    str_pos[2][1],str_pos[2][2],str_pos[2][3],str_pos[2][4],str_pos[2][5],str_pos[2][6],str_pos[2][7],str_pos[2][8],str_pos[2][9],str_pos[2][10],str_pos[2][11]
+    ))
+    # G
+    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
+    get_guitar_note_from_int(4),
+    #str_pos[3][0],
+    str_pos[3][1],str_pos[3][2],str_pos[3][3],str_pos[3][4],str_pos[3][5],str_pos[3][6],str_pos[3][7],str_pos[3][8],str_pos[3][9],str_pos[3][10],str_pos[3][11]
+    ))
+    # B
+    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
+    get_guitar_note_from_int(5),
+    #str_pos[4][0],
+    str_pos[4][1],str_pos[4][2],str_pos[4][3],str_pos[4][4],str_pos[4][5],str_pos[4][6],str_pos[4][7],str_pos[4][8],str_pos[4][9],str_pos[4][10],str_pos[4][11]
+    ))
+    # E
+    print("{}  |{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(
+    get_guitar_note_from_int(6),
+    #str_pos[5][0],
+    str_pos[5][1],str_pos[5][2],str_pos[5][3],str_pos[5][4],str_pos[5][5],str_pos[5][6],str_pos[5][7],str_pos[5][8],str_pos[5][9],str_pos[5][10],str_pos[5][11]
+    ))
+    # print the help bullets of the guitar
+    print("                       o               o               o               o              o o   ")
 
 trainer_active = True
 while (trainer_active):
     print("Welcome to the Python Guitar Note Trainer.")
     print("")
     print("")
-    
+
     # Define a random position on the guitar neck
     guitar_string = randint(1,6)
     guitar_fret = randint(0,12)
@@ -143,13 +189,16 @@ while (trainer_active):
         print("Please try to convert the following constellation: Guitar String: {}, Guitar Fret: {}".format(guitar_string, guitar_fret))
     draw_fretboard(guitar_string,guitar_fret)
     print("")
-    user_guess = input("What do you think this Note is: ")
+    user_guess = input("What do you think this Note is (or type a command): ")
 
     if (user_guess.upper() == "Q") or (user_guess.upper() == "QUIT"):
         trainer_active = False
+    elif (user_guess.upper() == "H") or (user_guess.upper() == "HELP"):
+        print("These are all the notes")
+        draw_fretboard_with_notes()
 
     if trainer_active:
-        starting_note = get_guitar_note_from_int(guitar_string)
+        starting_note = get_guitar_note_from_int(guitar_string).upper()
         # DEBUG:
         #print("starting_note: {}".format(starting_note))
         correct_guess = get_correct_guitar_note(starting_note,guitar_fret)
